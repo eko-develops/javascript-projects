8ab889b22f731ae9f90e84c65ef1a4f2c233b5bb
eko-develops <ekodevelops@gmail.com>
2021-04-01 23:58:00 -0700

    Added styling and values to the DOM

--- user-card/app.js
+++ user-card/app.js
@@ -1,19 +1,49 @@
-const onLoad =  () => {
-        fetch('https://randomuser.me/api/')
+const getUserData = () => {
+        const data = fetch('https://randomuser.me/api/')
         .then( response => {
             return response.json();
         })
         .then( data => {
             const { results } = data;
-
-            //We do not need to spread the array index because it is already an object
+            //We do not need to spread the array index because it is already an object.
+            //Just assign the index of the array (the object) to a variable for easy handling.
             // const { ...result} = results[0];
             const result = results[0];
-            console.log(result);
+            return result;
         })
         .catch( err => {
-            console.log(err);
+            return `FETCH ERROR IN GETUSERDATA(): ${err}`;
         })
+        return data;
 }
 
-window.addEventListener('DOMContentLoaded', onLoad);
+const displayUser = () => {
+    const fetchData = getUserData();
+    // console.log(fetchData);
+    fetchData.then( data => {
+        console.log(data);
+        
+        const { picture, name, location, email, phone } = data;
+
+        const userImg = document.querySelector('.user-image');
+        const userName = document.querySelector('.user-name');
+        const userLocation = document.querySelector('.location');
+        const userEmail = document.querySelector('.email');
+        const userPhone = document.querySelector('.phone');
+        
+        userImg.src = picture.large;
+        
+        userName.textContent = `${name.first} ${name.last}`;
+
+        userLocation.textContent = `${location.city}, ${location.state}, ${location.country}`;
+        
+
+        userEmail.textContent = email;
+
+        userPhone.textContent = `Tel: ${phone}`;
+    })
+}
+
+
+
+window.addEventListener('DOMContentLoaded', displayUser);
